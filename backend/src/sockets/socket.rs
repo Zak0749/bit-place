413077f0b4fe3032881759ba3d2bbc894da58dfe
Zak <89150707+Zak0749@users.noreply.github.com>
2022-05-14 12:14:39 +0100

    removed irrelevent  comments

--- backend/src/sockets/socket.rs
+++ backend/src/sockets/socket.rs
@@ -8,7 +8,6 @@ impl Actor for Socket {
     type Context = ws::WebsocketContext<Self>;
 }
 
-/// Handler for ws::Message message
 impl StreamHandler<Result<ws::Message, ws::ProtocolError>> for Socket {
     fn handle(&mut self, msg: Result<ws::Message, ws::ProtocolError>, ctx: &mut Self::Context) {
         if let Ok(Text(text)) = msg {
@@ -19,7 +18,6 @@ impl StreamHandler<Result<ws::Message, ws::ProtocolError>> for Socket {
 }
 
 #[get("")]
-// pub async fn route() -> impl Responder {
 pub async fn route(req: HttpRequest, stream: web::Payload) -> Result<HttpResponse, Error> {
     ws::start(Socket {}, &req, stream)
 }
