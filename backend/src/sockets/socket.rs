cf846b8345372ee69b629874e3442396fd3fae5c
Zak <89150707+Zak0749@users.noreply.github.com>
2022-05-14 13:49:14 +0100

    Changed Socket name

--- backend/src/sockets/socket.rs
+++ backend/src/sockets/socket.rs
@@ -2,13 +2,13 @@ use actix::{Actor, StreamHandler};
 use actix_web::{get, web, Error, HttpRequest, HttpResponse};
 use actix_web_actors::{ws, ws::Message::Text};
 
-pub struct Socket;
+pub struct Session;
 
-impl Actor for Socket {
+impl Actor for Session {
     type Context = ws::WebsocketContext<Self>;
 }
 
-impl StreamHandler<Result<ws::Message, ws::ProtocolError>> for Socket {
+impl StreamHandler<Result<ws::Message, ws::ProtocolError>> for Session {
     fn handle(&mut self, msg: Result<ws::Message, ws::ProtocolError>, ctx: &mut Self::Context) {
         if let Ok(Text(text)) = msg {
             println!("Text: {}", text);
@@ -19,5 +19,5 @@ impl StreamHandler<Result<ws::Message, ws::ProtocolError>> for Socket {
 
 #[get("")]
 pub async fn route(req: HttpRequest, stream: web::Payload) -> Result<HttpResponse, Error> {
-    ws::start(Socket {}, &req, stream)
+    ws::start(Session {}, &req, stream)
 }
