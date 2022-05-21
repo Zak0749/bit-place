9c0e969976c40d90d90eea8eff2d9041524ab230
Zak <89150707+Zak0749@users.noreply.github.com>
2022-05-22 00:24:50 +0100

    web sockets

---
+++ backend/src/api/point.rs
@@ -0,0 +1,32 @@
+use actix::Addr;
+use actix_redis::{Command, RedisActor, RespValue};
+use actix_web::{error, get, web, web::Data, HttpResponse, Result};
+use redis_async::resp_array;
+use serde::Deserialize;
+
+#[derive(Deserialize, Debug)]
+pub struct Point {
+    x: i32,
+    y: i32,
+}
+
+#[get("/point/{x}/{y}")] // , redis: Data<Addr<RedisActor>>
+pub async fn route(redis: Data<Addr<RedisActor>>, point: web::Path<Point>) -> Result<HttpResponse> {
+    log::info!("{:?}", point);
+    // Ok(HttpResponse::Ok().body(format!("{:?}", point)))
+    let data = redis
+        .send(Command(resp_array![
+            "GET",
+            format!("{}:{}", point.x, point.y)
+        ]))
+        .await
+        .map_err(error::ErrorInternalServerError)?
+        .map_err(error::ErrorInternalServerError)?;
+
+    if let RespValue::BulkString(result) = data {
+        Ok(HttpResponse::Ok().body(result))
+    } else {
+        println!("error: {:?}", data);
+        Ok(HttpResponse::InternalServerError().body("bad type"))
+    }
+}
