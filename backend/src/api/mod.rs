375f2e1460374fc1890c5daed69993c813c024a1
Zak <89150707+Zak0749@users.noreply.github.com>
2022-05-24 19:04:09 +0100

    get rid of testing code

--- backend/src/api/mod.rs
+++ backend/src/api/mod.rs
@@ -1,11 +1,9 @@
 use actix_web::web;
 mod grid;
-mod message;
 mod point;
 
 pub fn routes() -> actix_web::Scope {
     web::scope("/api")
-        .service(message::route)
         .service(point::route)
         .service(grid::route)
 }
